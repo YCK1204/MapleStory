bd23bf5daa9187eb8cf2aa9c072eb3fe63f33f18
Yechan kim <dpcks5959@naver.com>
2025-02-10 18:53:48 +0900

    [REFACTOR] CharacterSelectHandler 코드 리팩토링

--- Server/Server/CreateCharacter.cpp
+++ Server/Server/CreateCharacter.cpp
@@ -56,62 +56,6 @@ void PacketHandler::D_CheckNameHandler(PacketSession* session, ByteRef& buffer)
 	}
 }
 
-void PacketHandler::C_CharacterSelectHandler(PacketSession* session, ByteRef& buffer)
-{
-	try {
-		auto pkt = GetRoot<C_CharacterSelect>(buffer->operator std::byte * ());
-		ClientRef client = Manager::Session.Find(session->GetSessionId());
-		if (client == nullptr)
-		{
-			session->Disconnect();
-			return;
-		}
-		auto charId = pkt->char_id();
-		auto sessionId = session->GetSessionId();
-		auto dbId = session->GetDbId();
-
-		FlatBufferBuilder builder;
-		auto data = CreateSD_CharacterSelect(builder, sessionId, dbId, charId);
-		auto packet = Manager::Packet.CreatePacket(data, builder, PacketType_SD_CharacterSelect);
-		Manager::Session.dbSession->Send(packet);
-	}
-	catch (exception& e)
-	{
-		FlatBufferBuilder builder;
-		auto data = CreateSC_CharacterSelect(builder, CharacterSelectError::CharacterSelectError_UNKNOWN);
-		auto packet = Manager::Packet.CreatePacket(data, builder, PacketType_SC_CharacterSelect);
-		session->Send(packet);
-	}
-}
-
-void PacketHandler::D_CharacterSelectHandler(PacketSession* session, ByteRef& buffer)
-{
-	auto pkt = GetRoot<D_CharacterSelect>(buffer->operator std::byte * ());
-
-	if (pkt->ok() != CharacterSelectError_SUCCESS)
-		return;
-	ClientRef client = Manager::Session.Find(pkt->session_id());
-	if (client == nullptr)
-		return;
-	client->Player = shared_ptr<Player>(new Player(client));
-	FlatBufferBuilder builder;
-
-	auto info = pkt->char_info();
-	auto prevInfo = info->prev_info();
-	auto ability = prevInfo->ability();
-	auto lastPos = info->last_pos();
-
-	client->Player->CurMapId = lastPos;
-
-	auto newAbility = CreateCharacterAbility(builder, ability->STR(), ability->DEX(), ability->INT(), ability->LUK());
-	auto newPrevInfo = CreateCharacterPreviewInfoDirect(builder, prevInfo->char_id(), prevInfo->char_type(), prevInfo->level(), prevInfo->name()->c_str());
-	auto newTotalinfo = CreateCharacterTotalInfo(builder, newPrevInfo, lastPos, info->hp(), info->mp(), info->exp());
-
-	auto data = CreateSC_CharacterSelect(builder, newTotalinfo);
-	auto packet = Manager::Packet.CreatePacket(data, builder, PacketType_SC_CharacterSelect);
-	client->Send(packet);
-}
-
 void PacketHandler::C_CreateCharacterHandler(PacketSession* session, ByteRef& buffer)
 {
 	auto pkt = GetRoot<C_CreateCharacter>(buffer->operator std::byte * ());
